343b327c73ebef039c1dfc2f615e68bf1243e216
33333m3 <m-murashita0278@st.kyoto-art.ac.jp>
2020-10-01 23:10:02 +0900

    １

---
+++ basic_practice/sample/js/script.js
@@ -0,0 +1,14 @@
+
+$(function(){
+  
+  $("#slide").slick({dots:true});
+   $（"#slide").sideToggle();
+})
+
+$("#slide").slick({
+   autoplay: true
+   autoplaySpeed: 2000,
+   dots:true
+  });
+
+});
